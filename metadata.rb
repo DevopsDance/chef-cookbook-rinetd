b4d2b75baf146c36ed0019be780adf85d0deba78
Marcin Wolny <marcin@wutanic.com>
2017-05-26 19:50:30 +0200

    Make Rubocop and Foodcritic happy!

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,12 @@ description 'Installs and configures ridentd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
 chef_version '~> 12'
-issue_url 'https://github.com/DevopsDance/chef-cookbook-ridentd/issues'
+issues_url 'https://github.com/DevopsDance/chef-cookbook-ridentd/issues'
 source_url 'https://github.com/DevopsDance/chef-cookbook-ridentd'
 
 supports 'ubuntu', '>= 14.04'
 supports 'debian', '>= 7.0'
 
+depends 'chef-sugar'
 depends 'ubuntu'
 depends 'debian'
