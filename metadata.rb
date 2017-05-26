03a165081fb7499ed7feea21ba822ce1cb58bd1c
Marcin Wolny <marcin@wutanic.com>
2017-05-26 19:21:02 +0200

    Support more distributions

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,13 @@ maintainer_email 'team@devops.dance'
 license 'Apache-2.0'
 description 'Installs and configures ridentd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 chef_version '~> 12'
 issue_url 'https://github.com/DevopsDance/chef-cookbook-ridentd/issues'
 source_url 'https://github.com/DevopsDance/chef-cookbook-ridentd'
 
+supports 'ubuntu', '>= 14.04'
+supports 'debian', '>= 7.0'
+
 depends 'ubuntu'
+depends 'debian'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,12 @@
-include_recipe 'ubuntu'
+include_recipe 'chef-sugar'
+
+if ubuntu?
+  include_recipe 'ubuntu'
+elsif debian?
+  include_recipe 'debian'
+else
+  raise 'Unsupported platform'
+end
 
 package 'rinetd' do
   action :install
