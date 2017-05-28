80e3f2c4d711cb4cbfb25fb4a3922dccda96a98f
Marcin Wolny <marcin@wutanic.com>
2017-05-28 20:54:22 +0200

    [BUGFIX] Action :disable does not work
        With this change action :disable does not add new configuration
        item.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'team@devops.dance'
 license 'Apache-2.0'
 description 'Installs and configures ridentd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.3.0'
 chef_version '~> 12'
 issues_url 'https://github.com/DevopsDance/chef-cookbook-ridentd/issues'
 source_url 'https://github.com/DevopsDance/chef-cookbook-ridentd'
